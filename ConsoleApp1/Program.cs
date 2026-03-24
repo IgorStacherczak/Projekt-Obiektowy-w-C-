b5483c75c78759ade94aada80ec653939fbd22ac
YooungIGI <2bqf2ygz6q@privaterelay.appleid.com>
2026-03-24 12:43:22 +0100

    Odzyskanie zmian po stash i konflikcie merge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,31 @@ class Program
         TypUzytkownika typUzytkownika;
         SerwisWypozyczalni serwis = new SerwisWypozyczalni();
         
+        Uzytkownik u1 = new Uzytkownik("Jan", "Kowalski", TypUzytkownika.Student);
+        Uzytkownik u2 = new Uzytkownik("Anna", "Nowak", TypUzytkownika.Pracownik);
+        Uzytkownik u3 = new Uzytkownik("Piotr", "Zielinski", TypUzytkownika.Student);
+
+        serwis.DodajUzytkownika(u1);
+        serwis.DodajUzytkownika(u2);
+        serwis.DodajUzytkownika(u3);
+        
+        Laptop l1 = new Laptop("Laptop", "i5", 8);
+        Laptop l2 = new Laptop("Laptop", "i7", 16);
+        Kamera k1 = new Kamera("Kamera", "4K", 60);
+        Projektor p1 = new Projektor("Projektor", 4000, "FullHD");
+
+        serwis.DodajSprzet(l1);
+        serwis.DodajSprzet(l2);
+        serwis.DodajSprzet(k1);
+        serwis.DodajSprzet(p1);
+
+        serwis.WypozyczSprzet(u1, l1);
+        serwis.WypozyczSprzet(u2, k1);
+
+        Wypozyczenie stare = new Wypozyczenie(u3, p1, DateTime.Today.AddDays(-10), DateTime.Today.AddDays(-3));
+        serwis.DodajWypozyczenie(stare);
+        serwis.ListaSprzetu[3].ZmienStatus(Status.Wypozyczony);
+        
         while (i != 0)
         {
             Console.WriteLine("1. Dodaj użytkownika\n" +
@@ -19,6 +44,10 @@ class Program
                               "4. Zwróć sprzęt\n" +
                               "5. Raport\n" +
                               "6. Wyświetl dostępny sprzęt\n" +
+                              "7. Wyświetl cały sprzęt\n" +
+                              "8. Aktywne wypożyczenia użytkownika\n" +
+                              "9. Przeterminowane wypożyczenia\n" +
+                              "10. Oznacz sprzęt jako uszkodzony\n" +
                               "0. Wyjście\n");
             Console.Write("Podaj numer akcji: ");
             i = Convert.ToInt32(Console.ReadLine());
@@ -90,23 +119,39 @@ class Program
 
                     int uIndex = Convert.ToInt32(Console.ReadLine());
 
+                    List<Sprzet> dostepneSprzety = new List<Sprzet>();
+
                     Console.WriteLine("Wybierz sprzęt:");
                     for (int j = 0; j < serwis.ListaSprzetu.Count; j++)
                     {
-                        Console.WriteLine(j + ". " + serwis.ListaSprzetu[j].NazwaSprzetu + " ID: " + serwis.ListaSprzetu[j].ID + " Status: " + serwis.ListaSprzetu[j].StatusSprzetu);
+                        if (serwis.ListaSprzetu[j].StatusSprzetu == Status.Dostepny)
+                        {
+                            dostepneSprzety.Add(serwis.ListaSprzetu[j]);
+                        }
+                    }
+
+                    if (dostepneSprzety.Count == 0)
+                    {
+                        Console.WriteLine("Brak dostępnego sprzętu");
+                        break;
+                    }
+
+                    for (int j = 0; j < dostepneSprzety.Count; j++)
+                    {
+                        Console.WriteLine(j + ". " + dostepneSprzety[j].NazwaSprzetu + " ID: " +
+                                          dostepneSprzety[j].ID + " Status: " +
+                                          dostepneSprzety[j].StatusSprzetu);
                     }
 
                     int sIndex = Convert.ToInt32(Console.ReadLine());
 
-                    serwis.WypozyczSprzet(serwis.ListaUzytkownikow[uIndex], serwis.ListaSprzetu[sIndex]);
-
-                    Console.WriteLine("Próba wypożyczenia wykonana.");
+                    serwis.WypozyczSprzet(serwis.ListaUzytkownikow[uIndex], dostepneSprzety[sIndex]);
+                    Console.WriteLine("Sprzęt został wypożyczony.");
                     break;
-
                 case 4:
-                    if (serwis.ListaUzytkownikow.Count == 0 || serwis.ListaSprzetu.Count == 0)
+                    if (serwis.ListaUzytkownikow.Count == 0)
                     {
-                        Console.WriteLine("Brak użytkowników lub sprzętu!");
+                        Console.WriteLine("Brak użytkowników!");
                         break;
                     }
 
@@ -117,24 +162,88 @@ class Program
                     }
 
                     int uIndex2 = Convert.ToInt32(Console.ReadLine());
+                    Uzytkownik wybranyUzytkownik = serwis.ListaUzytkownikow[uIndex2];
 
-                    Console.WriteLine("Wybierz sprzęt:");
+                    List<Sprzet> wypozyczoneSprzetyUzytkownika = new List<Sprzet>();
+
                     for (int j = 0; j < serwis.ListaSprzetu.Count; j++)
                     {
-                        Console.WriteLine(j + ". " + serwis.ListaSprzetu[j].NazwaSprzetu + " ID: " + serwis.ListaSprzetu[j].ID);
+                        for (int k = 0; k < serwis.ListaWypozyczen.Count; k++)
+                        {
+                            if (serwis.ListaWypozyczen[k].Uzytkownik == wybranyUzytkownik &&
+                                serwis.ListaWypozyczen[k].Sprzet == serwis.ListaSprzetu[j] &&
+                                serwis.ListaWypozyczen[k].DataFaktycznegoZwrotu == null)
+                            {
+                                wypozyczoneSprzetyUzytkownika.Add(serwis.ListaSprzetu[j]);
+                            }
+                        }
+                    }
+
+                    if (wypozyczoneSprzetyUzytkownika.Count == 0)
+                    {
+                        Console.WriteLine("Ten użytkownik nie ma aktywnych wypożyczeń.");
+                        break;
+                    }
+
+                    Console.WriteLine("Wybierz sprzęt do zwrotu:");
+                    for (int j = 0; j < wypozyczoneSprzetyUzytkownika.Count; j++)
+                    {
+                        Console.WriteLine(j + ". " + wypozyczoneSprzetyUzytkownika[j].NazwaSprzetu +
+                                          " ID: " + wypozyczoneSprzetyUzytkownika[j].ID);
                     }
 
                     int sIndex2 = Convert.ToInt32(Console.ReadLine());
 
-                    serwis.ZwrotSprzetu(serwis.ListaUzytkownikow[uIndex2], serwis.ListaSprzetu[sIndex2]);
+                    serwis.ZwrotSprzetu(wybranyUzytkownik, wypozyczoneSprzetyUzytkownika[sIndex2]);
 
-                    Console.WriteLine("Próba zwrotu wykonana.");
+                    Console.WriteLine("Zwrot wykonany.");
                     break;
                 case 5:
                     serwis.WyswietlRaport();
                     break;
                 case 6:
                     serwis.WyswietlDostepnySprzet();
+                    break;
+                case 7:
+                    serwis.WyswietlWszystkieSprzety();
+                    break;
+                case 8:
+                    if (serwis.ListaUzytkownikow.Count == 0)
+                    {
+                        Console.WriteLine("Brak użytkowników!");
+                        break;
+                    }
+
+                    Console.WriteLine("Wybierz użytkownika:");
+                    for (int j = 0; j < serwis.ListaUzytkownikow.Count; j++)
+                    {
+                        Console.WriteLine(j + ". " + serwis.ListaUzytkownikow[j].Imie + " " + serwis.ListaUzytkownikow[j].Nazwisko);
+                    }
+
+                    int uIndex3 = Convert.ToInt32(Console.ReadLine());
+                    serwis.WyswietlAktywneWypozyczeniaUzytkownika(serwis.ListaUzytkownikow[uIndex3]);
+                    break;
+                case 9:
+                    serwis.WysietlPrzeterminowane();
+                    break;
+                case 10:
+                    if (serwis.ListaSprzetu.Count == 0)
+                    {
+                        Console.WriteLine("Brak sprzętu!");
+                        break;
+                    }
+
+                    Console.WriteLine("Wybierz sprzęt do oznaczenia jako uszkodzony:");
+                    for (int j = 0; j < serwis.ListaSprzetu.Count; j++)
+                    {
+                        Console.WriteLine(j + ". " + serwis.ListaSprzetu[j].NazwaSprzetu +
+                                          " ID: " + serwis.ListaSprzetu[j].ID +
+                                          " Status: " + serwis.ListaSprzetu[j].StatusSprzetu);
+                    }
+
+                    int sIndex3 = Convert.ToInt32(Console.ReadLine());
+                    serwis.OznaczJakoUszkodzony(serwis.ListaSprzetu[sIndex3]);
+                    Console.WriteLine("Sprzęt został oznaczony jako uszkodzony.");
                     break;
             }
         }
--- ConsoleApp1/SerwisWypozyczalni.cs
+++ ConsoleApp1/SerwisWypozyczalni.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices.JavaScript;
+
 namespace ConsoleApp1;
 using System.Collections.Generic;
 
@@ -7,37 +9,52 @@ public class SerwisWypozyczalni
     private List<Uzytkownik> listaUzytkownikow = new List<Uzytkownik>();
     private List<Wypozyczenie> listaWypozyczen = new List<Wypozyczenie>();
 
-    public void WypozyczSprzet(Uzytkownik uzytkownik,Sprzet sprzet)
+    public void WypozyczSprzet(Uzytkownik uzytkownik, Sprzet sprzet)
     {
-        if (uzytkownik != null)
+        if (uzytkownik == null)
         {
-            
-            int liczbaAktywnychWypozyczen = 0;
-            bool czyMoze = false;
+            Console.WriteLine("Brak użytkownika.");
+            return;
+        }
 
-            for (int i = 0; i < listaWypozyczen.Count; i++)
-            {
-                if (listaWypozyczen[i].Uzytkownik == uzytkownik  && listaWypozyczen[i].DataFaktycznegoZwrotu == null)
-                {
-                    liczbaAktywnychWypozyczen++;
-                }
-            }
+        int liczbaAktywnychWypozyczen = 0;
 
-            if (liczbaAktywnychWypozyczen < 2 && uzytkownik.TypUzytkownika == TypUzytkownika.Student)
+        for (int i = 0; i < listaWypozyczen.Count; i++)
+        {
+            if (listaWypozyczen[i].Uzytkownik == uzytkownik &&
+                listaWypozyczen[i].DataFaktycznegoZwrotu == null)
             {
-                czyMoze = true;
-            }else if (liczbaAktywnychWypozyczen < 5 && uzytkownik.TypUzytkownika == TypUzytkownika.Pracownik)
-            {
-                czyMoze = true;
-            }
-            
-            if (sprzet != null && sprzet.StatusSprzetu == Status.Dostepny && czyMoze == true)
-            {
-                Wypozyczenie wypozyczenie = new Wypozyczenie(uzytkownik, sprzet, DateTime.Today, DateTime.Today.AddDays(7));
-                listaWypozyczen.Add(wypozyczenie);
-                sprzet.ZmienStatus(Status.Wypozyczony);
+                liczbaAktywnychWypozyczen++;
             }
         }
+
+        if (uzytkownik.TypUzytkownika == TypUzytkownika.Student && liczbaAktywnychWypozyczen >= 2)
+        {
+            Console.WriteLine("Student może mieć maksymalnie 2 aktywne wypożyczenia.");
+            return;
+        }
+
+        if (uzytkownik.TypUzytkownika == TypUzytkownika.Pracownik && liczbaAktywnychWypozyczen >= 5)
+        {
+            Console.WriteLine("Pracownik może mieć maksymalnie 5 aktywnych wypożyczeń.");
+            return;
+        }
+
+        if (sprzet == null)
+        {
+            Console.WriteLine("Brak sprzętu.");
+            return;
+        }
+
+        if (sprzet.StatusSprzetu != Status.Dostepny)
+        {
+            Console.WriteLine("Sprzęt jest niedostępny.");
+            return;
+        }
+
+        Wypozyczenie wypozyczenie = new Wypozyczenie(uzytkownik, sprzet, DateTime.Today, DateTime.Today.AddDays(7));
+        listaWypozyczen.Add(wypozyczenie);
+        sprzet.ZmienStatus(Status.Wypozyczony);
     }
 
     public List<Sprzet> ListaSprzetu
@@ -45,6 +62,14 @@ public class SerwisWypozyczalni
         get
         {
             return listasprzetu;
+        }
+    }
+    
+    public List<Wypozyczenie> ListaWypozyczen
+    {
+        get
+        {
+            return listaWypozyczen;
         }
     }
     
@@ -76,6 +101,15 @@ public class SerwisWypozyczalni
                     listaWypozyczen[i].KwotaKary = kara;
                 }
                 
+                if (kara > 0)
+                {
+                    Console.WriteLine("Kara za spóźnienie: " + kara + " zł");
+                }
+                else
+                {
+                    Console.WriteLine("Brak kary");
+                }
+                
                 sprzet.ZmienStatus(Status.Dostepny);
                 break;
             }
@@ -105,7 +139,7 @@ public class SerwisWypozyczalni
         {
             if (listaWypozyczen[i].DataFaktycznegoZwrotu == null)
             {
-                Console.WriteLine("Nazwa Osoby: " + listaWypozyczen[i].Uzytkownik.Imie + listaWypozyczen[i].Uzytkownik.Nazwisko +
+                Console.WriteLine("Nazwa Osoby: " + listaWypozyczen[i].Uzytkownik.Imie + " " + listaWypozyczen[i].Uzytkownik.Nazwisko +
                                   " Sprzęt: " + listaWypozyczen[i].Sprzet.NazwaSprzetu + " Sprzęt ID: " + listaWypozyczen[i].Sprzet.ID +
                                   " Data Wypożyczenia: " +  listaWypozyczen[i].DataWypozyczenia.ToShortDateString() + 
                                   " Data Zwrotu: " + listaWypozyczen[i].DataZwrotu.ToShortDateString());
@@ -146,20 +180,83 @@ public class SerwisWypozyczalni
 
     public void WyswietlDostepnySprzet()
     {
+        
+        bool znaleziono = false;
+        
         for (int i = 0; i < listasprzetu.Count; i++)
         {
             if (listasprzetu[i].StatusSprzetu == Status.Dostepny)
             {
                 Console.WriteLine("Nazwa: " + listasprzetu[i].NazwaSprzetu + " ID: " + listasprzetu[i].ID + " Status: " + listasprzetu[i].StatusSprzetu);
-            }else
-            {
-                Console.WriteLine("Brak dostępnego sprzętu");
+                znaleziono = true;
             }
         }
 
-        if (listasprzetu.Count == 0)
+        if (listasprzetu.Count == 0 || znaleziono == false)
         {
             Console.WriteLine("Brak dostępnego sprzętu");
+        }
+    }
+
+    public void WyswietlWszystkieSprzety()
+    {
+        for (int i = 0; i < listasprzetu.Count; i++)
+        {
+            Console.WriteLine("Nazwa: " + listasprzetu[i].NazwaSprzetu + " ID: " + listasprzetu[i].ID + " Status: " + listasprzetu[i].StatusSprzetu);
+        }
+    }
+    
+    public void WysietlPrzeterminowane()
+    {
+        
+        bool brak = true;
+        
+        for (int i = 0; i < listaWypozyczen.Count; i++)
+        {
+            if (listaWypozyczen[i].DataZwrotu < DateTime.Today && listaWypozyczen[i].DataFaktycznegoZwrotu == null)
+            {
+                Console.WriteLine("Sprzęt: " + listaWypozyczen[i].Sprzet.NazwaSprzetu + 
+                                  " Imię: " + listaWypozyczen[i].Uzytkownik.Imie + 
+                                  " Nazwisko: " + listaWypozyczen[i].Uzytkownik.Nazwisko + 
+                                  " Data Zwrotu: " + listaWypozyczen[i].DataZwrotu);
+                brak = false;
+            }
+        }
+
+        if (brak)
+        {
+            Console.WriteLine("Brak Przeterminowanych");
+        }
+    }
+    
+    public void WyswietlAktywneWypozyczeniaUzytkownika(Uzytkownik uzytkownik)
+    {
+        bool brak = true;
+
+        for (int i = 0; i < listaWypozyczen.Count; i++)
+        {
+            if (listaWypozyczen[i].Uzytkownik == uzytkownik &&
+                listaWypozyczen[i].DataFaktycznegoZwrotu == null)
+            {
+                Console.WriteLine("Sprzęt: " + listaWypozyczen[i].Sprzet.NazwaSprzetu +
+                                  " ID: " + listaWypozyczen[i].Sprzet.ID +
+                                  " Data wypożyczenia: " + listaWypozyczen[i].DataWypozyczenia.ToShortDateString() +
+                                  " Data zwrotu: " + listaWypozyczen[i].DataZwrotu.ToShortDateString());
+                brak = false;
+            }
+        }
+
+        if (brak)
+        {
+            Console.WriteLine("Brak aktywnych wypożyczeń tego użytkownika");
+        }
+    }
+    
+    public void DodajWypozyczenie(Wypozyczenie wypozyczenie)
+    {
+        if (wypozyczenie != null)
+        {
+            listaWypozyczen.Add(wypozyczenie);
         }
     }
 }
